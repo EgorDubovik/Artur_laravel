9c07f24f0f0578c9e6011b94ab0f1b7126b58cb6
EgorDubovik <posik.web.m@gmail.com>
2020-11-15 18:06:35 -0600

    add files

---
+++ app/Payments.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payments extends Model
+{
+    //
+    protected $table = "payments";
+    public const PENDING = 'PENDING';
+    public const PAID = 'PAID';
+    protected $fillable = [
+    	'amount',
+    	'status',
+    	'user_id',
+    ];
+}
