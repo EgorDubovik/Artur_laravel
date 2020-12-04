b17fc62edc1156bd366607d69d23b4ba6605d3d3
EgorDubovik <posik.web.m@gmail.com>
2020-12-03 22:26:23 -0600

    create migrate

---
+++ database/migrations/2020_12_04_042059_create_list_service_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list_service', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->integer('id_prefix')->nullable();
+            $table->integer('price')->nullable();
+            $table->integer('id_service')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list_service');
+    }
+}
