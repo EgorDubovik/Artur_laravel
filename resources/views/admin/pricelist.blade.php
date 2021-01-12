b9e6f4bbd669f2aaf69741374e83aded7701a0e0
EgorDubovik <posik.web.m@gmail.com>
2021-01-11 21:31:30 -0600

    view table price list

--- resources/views/admin/pricelist.blade.php
+++ resources/views/admin/pricelist.blade.php
@@ -7,26 +7,50 @@
     
    
     <div class="row">
-    	<div class="col-xl-8">
+    	<div class="col-xl-12">
             <!-- Account details card-->
             <div class="card mb-4">
                 <div class="card-header">Account Details</div>
                 <div class="card-body">
-                    <ul>
+                    
                     @foreach($services as $service)
-                        <li>
-                            {{$service->title}}
-                        </li>
-
+                        <table class="table table-borderless mb-4">
+                            <thead>
+                                <tr class="small text-uppercase text-muted">
+                                    <th style="width: 75%">{{$service->title}}</th>
+                                    <th style="width: 15%">price</th>
+                                    <th style="width: 10%">action</th>
+                                </tr>
+                            </thead>
+                            
+                            @if($service->pod_services)
+                            <tbody>
+                                @foreach($service->pod_services as $pod_service)
+                                    <tr class="border-bottom" data-id="{{$pod_service->id}}">
+                                        <td><span style="margin-left: 20px">{{$pod_service->title}}</td>
+                                        <td>{{$pod_service->price}}</td>
+                                        <td></td>
+                                    </tr>
+                                    @if($pod_service->pod_services)
+                                        @foreach($pod_service->pod_services as 
+                                        $dop_service)
+                                        <tr>
+                                            <td><span style="margin-left: 40px;"> {{$dop_service->title}}</span></td>
+                                            <td>{{$dop_service->price}}</td>
+                                            <td>
+                                                <button class="btn btn-datatable btn-icon btn-transparent-dark mr-2"><i class="fas fa-edit"></i></button>
+                                                <a href="#" onclick="if(confirm('Are you sure you want to deactivate it')) return true; else return false;" class="btn btn-datatable btn-icon btn-transparent-dark"><i class="fas fa-trash-alt"></i></a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    @endif
+                                @endforeach
+                                
+                            </tbody>
+                            @endif
+                            
+                        </table>
                     @endforeach
-                    <ul>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-4">   
-            <div class="card mb-4">
-                <div class="card-header">Information</div>
-                <div class="card-body">
                     
                 </div>
             </div>
