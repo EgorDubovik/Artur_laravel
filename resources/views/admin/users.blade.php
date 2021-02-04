472a06b5c343a8456bf53297f80ad2483d07c7e9
EgorDubovik <posik.web.m@gmail.com>
2021-02-04 14:19:56 -0600

    add view user id for admins

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -12,6 +12,7 @@
 				<table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
 					<thead>
 						<tr>
+							<th>Id</th>
 							<th>Name</th>
 							<th>Email</th>
 							<th>Start date</th>
@@ -22,6 +23,7 @@
 					</thead>
 					<tfoot>
 						<tr>
+							<th>Id</th>
 							<th>Name</th>
 							<th>Email</th>
 							<th>Start date</th>
@@ -33,6 +35,7 @@
 					<tbody>
 						@foreach($users as $auser)
 						<tr>
+							<td>{{$auser->id}}</td>
 							<td><a href="/admin/user/{{$auser->id}}" style="color: #585858;font-weight: bold"> {{$auser->first_name}} {{$auser->last_name}}</a></td>
 							<td>{{$auser->email}}</td>
 							<td>{{$auser->created_at}}</td>
