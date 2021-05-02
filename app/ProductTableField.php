1798cfa5aad600e74046b2f48d151954c4dba3a7
EgorDubovik <posik.web.m@gmail.com>
2021-05-02 09:39:51 -0500

    Create models

--- app/ProductTableField.php
+++ app/ProductTableField.php
@@ -5,18 +5,15 @@ namespace App;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class ProductTableField extends Model
 {
     use HasFactory;
-
     protected $table = 'product_table';
 
     protected $fillable = [
-    	'user_id'
+    	'table_id',
+    	'title'
     ]
 
 
-    function field(){
-    	return $this->hasMany('App\ProductTableField');
-    }
 }
---
+++ database/migrations/2021_05_02_142305_create_product_table_field.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTableField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_table_field', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->unsignedBigInteger('table_id');
+
+            $table->foreign('table_id')
+                ->references('id')
+                ->on('product_table')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_table_field');
+    }
+}
