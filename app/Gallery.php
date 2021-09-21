720db2ee706587728fad5139d1ecee50b9105a67
EgorDubovik <posik.web.m@gmail.com>
2021-09-20 19:04:07 -0500

    SAve file in database

--- app/Gallery.php
+++ app/Gallery.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gallery extends Model
 {
-    use HasFactory;
+    protected $table = "images";
+
+    protected $fillable = [
+    	'name',
+    	'is_active',
+    ];
 }
--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Gallery;
 
 class GalleryController extends Controller
 {
@@ -16,7 +17,14 @@ class GalleryController extends Controller
 
     public function upload(Request $request){
     	$file = $request->file('file');
-    	$file->move($this->folderPath,$file->getClientOriginalName());
+    	$new_name = getRandomFileName(50).'.'.$file->extension();
+
+    	Gallery::create([
+    		'name'=>$new_name,
+    		'is_active'=>1
+    	]);
+
+    	$file->move($this->folderPath,$new_name);
 		return back()->with('success','File uploaded successfully');
     }
 }
